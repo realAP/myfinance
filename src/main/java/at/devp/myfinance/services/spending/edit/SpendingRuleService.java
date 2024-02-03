e58c64038f3dc571977253335090c482145dfca1
ap <devp@posteo.de>
2024-02-03 11:28:39 +0100

    add transactional annotation

--- src/main/java/at/devp/myfinance/services/spending/edit/SpendingRuleService.java
+++ src/main/java/at/devp/myfinance/services/spending/edit/SpendingRuleService.java
@@ -4,6 +4,7 @@ import at.devp.myfinance.dto.SpendingEditDto;
 import at.devp.myfinance.entity.Spending;
 import at.devp.myfinance.repositories.RuleRepository;
 import at.devp.myfinance.services.rule.RuleEditService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ public class SpendingRuleService {
   private final RuleEditService ruleEditService;
   private final RuleRepository ruleRepository;
 
+  @Transactional
   public void setRule(SpendingEditDto spendingEditDto, Spending spending) {
     final var selectedRule = ruleRepository.findById(spendingEditDto.getRuleId()).orElseThrow(() -> new IllegalArgumentException("Rule with id " + spendingEditDto.getRuleId() + " not found"));
     final var oldRule = spending.getRule();
--- src/main/java/at/devp/myfinance/services/spending/edit/SpendingTransferService.java
+++ src/main/java/at/devp/myfinance/services/spending/edit/SpendingTransferService.java
@@ -4,6 +4,7 @@ import at.devp.myfinance.dto.SpendingEditDto;
 import at.devp.myfinance.entity.Spending;
 import at.devp.myfinance.repositories.TransferRepository;
 import at.devp.myfinance.services.transfer.TransferEditService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class SpendingTransferService {
   private final TransferRepository transferRepository;
   private final TransferEditService transferEditService;
 
+  @Transactional
   public void editSpendingTransfer(SpendingEditDto spendingEditDto, Spending spending) {
     final var oldTransfer = spending.getTransfer();
     final var selectedTransfer = transferRepository.findById(spendingEditDto.getTransferId()).orElseThrow(() -> new IllegalArgumentException("Transfer with id " + spendingEditDto.getTransferId() + " not found"));
