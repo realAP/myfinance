695afde3f3f6aed5c0871a1a745fae7ba8a2b07b
Axel Schwab <axel.schwab@pep-digital.de>
2024-08-10 12:22:24 +0200

    add test to satisfy test coverage

--- src/main/java/at/devp/myfinance/controller_fe/read/SpendingOverviewControllerFe.java
+++ src/main/java/at/devp/myfinance/controller_fe/read/SpendingOverviewControllerFe.java
@@ -16,43 +16,44 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SpendingOverviewControllerFe {
 
-  private final CategoryService categoryService;
-  private final RuleDropDownService ruleDropDownService;
+  //private final CategoryService categoryService;
+  //private final RuleDropDownService ruleDropDownService;
   private final SpendingOverviewService spendingOverviewService;
-  private final TransferDropDownService transferDropDownService;
+  //private final TransferDropDownService transferDropDownService;
 
 
   @GetMapping("/fe/overview")
   @CrossOrigin(origins = "http://localhost:4200")
   public List<SpendingCategoryBlockDto> getFinanceOverview() {
-    final var spendingCategoryBlockDtos = spendingOverviewService.createOverview();
+
+      // TODO: clean this when extending the creation controller
 /*
     model.addAttribute("spendingCategoryBlockDtos", spendingCategoryBlockDtos);
     model.addAttribute("spendingCategoryBlockDto", new SpendingCategoryBlockDto());
     model.addAttribute("spendingRowDto", new SpendingRowDto());
 */
-    final var categoryDtos = categoryService.createCategories();
+    //final var categoryDtos = categoryService.createCategories();
 /*
     model.addAttribute("categoryDtos", categoryDtos);
     model.addAttribute("spendingCreationDto", new SpendingCreationDto());
 */
 
-    final var sumOfSpendings = spendingOverviewService.calculateSum();
+    //final var sumOfSpendings = spendingOverviewService.calculateSum();
     //model.addAttribute("sumOfSpendings", sumOfSpendings);
 
 
-    final var ruleDropDownDtos = ruleDropDownService.createRuleDropDownDto();
+    //final var ruleDropDownDtos = ruleDropDownService.createRuleDropDownDto();
 /*
     model.addAttribute("ruleDropDownDtos", ruleDropDownDtos);
     model.addAttribute("ruleDropDownDto", new RuleDropDownDto());
 */
 
-    final var transferDropDownDtos = transferDropDownService.createDropDownDtos();
+    //final var transferDropDownDtos = transferDropDownService.createDropDownDtos();
 /*
     model.addAttribute("transferDropDownDtos", transferDropDownDtos);
     model.addAttribute("transferDropDownDto", new TransferDropDownDto());
 */
 
-    return spendingCategoryBlockDtos;
+    return spendingOverviewService.createOverview();
   }
 }
