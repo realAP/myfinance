92296dcafaf8113b2cb1a5c01db7207a5868a827
Axel Schwab <axel.schwab@pep-digital.de>
2024-08-10 12:29:24 +0200

    remove comments

--- src/main/java/at/devp/myfinance/controller_fe/read/SpendingOverviewControllerFe.java
+++ src/main/java/at/devp/myfinance/controller_fe/read/SpendingOverviewControllerFe.java
@@ -1,10 +1,7 @@
 package at.devp.myfinance.controller_fe.read;
 
-import at.devp.myfinance.services.CategoryService;
 import at.devp.myfinance.services.financeoverview.SpendingCategoryBlockDto;
 import at.devp.myfinance.services.financeoverview.SpendingOverviewService;
-import at.devp.myfinance.services.rule.RuleDropDownService;
-import at.devp.myfinance.services.transfer.TransferDropDownService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,44 +13,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SpendingOverviewControllerFe {
 
-  //private final CategoryService categoryService;
-  //private final RuleDropDownService ruleDropDownService;
-  private final SpendingOverviewService spendingOverviewService;
-  //private final TransferDropDownService transferDropDownService;
+    private final SpendingOverviewService spendingOverviewService;
 
-
-  @GetMapping("/fe/overview")
-  @CrossOrigin(origins = "http://localhost:4200")
-  public List<SpendingCategoryBlockDto> getFinanceOverview() {
-
-      // TODO: clean this when extending the creation controller
-/*
-    model.addAttribute("spendingCategoryBlockDtos", spendingCategoryBlockDtos);
-    model.addAttribute("spendingCategoryBlockDto", new SpendingCategoryBlockDto());
-    model.addAttribute("spendingRowDto", new SpendingRowDto());
-*/
-    //final var categoryDtos = categoryService.createCategories();
-/*
-    model.addAttribute("categoryDtos", categoryDtos);
-    model.addAttribute("spendingCreationDto", new SpendingCreationDto());
-*/
-
-    //final var sumOfSpendings = spendingOverviewService.calculateSum();
-    //model.addAttribute("sumOfSpendings", sumOfSpendings);
-
-
-    //final var ruleDropDownDtos = ruleDropDownService.createRuleDropDownDto();
-/*
-    model.addAttribute("ruleDropDownDtos", ruleDropDownDtos);
-    model.addAttribute("ruleDropDownDto", new RuleDropDownDto());
-*/
-
-    //final var transferDropDownDtos = transferDropDownService.createDropDownDtos();
-/*
-    model.addAttribute("transferDropDownDtos", transferDropDownDtos);
-    model.addAttribute("transferDropDownDto", new TransferDropDownDto());
-*/
-
-    return spendingOverviewService.createOverview();
-  }
+    @GetMapping("/fe/overview")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<SpendingCategoryBlockDto> getFinanceOverview() {
+        return spendingOverviewService.createOverview();
+    }
 }
