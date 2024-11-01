98d87b556abf819ba4b1ee313034216b5c7f7636
realAP <devp@posteo.de>
2024-11-01 15:56:15 +0100

    change test infra

--- fe/src/environments/environment.ts
+++ fe/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://financeapi.testp.de'  // Production API URL
+  apiUrl: 'https://financeapi.test.devp.at'  // Production API URL
 };
