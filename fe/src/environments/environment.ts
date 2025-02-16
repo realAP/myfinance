b0745caae7082a6b6a7d7e10826ad26b3d8344ca
ap <devp@posteo.de>
2025-02-16 14:05:54 +0100

    add test configuration for Angular and update Docker compose for testing

--- fe/src/environments/environment.ts
+++ fe/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://financeapi.test.devp.at'  // Production API URL
+  apiUrl: 'https://financeapi.devp.at'  // Production API URL
 };
